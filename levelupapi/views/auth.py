596f266d38169ac8b872b2b31525f2863844e041
Brandon De Leon <brandondeleon.615@gmail.com>
2021-11-03 14:43:42 -0500

    created an auth view

---
+++ levelupapi/views/auth.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from levelupapi.models import Gamer
