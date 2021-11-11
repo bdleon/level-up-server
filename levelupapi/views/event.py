88fdda46b0a06ce8e2cc23ad40d5014d400e1040
Brandon De Leon <brandondeleon.615@gmail.com>
2021-11-11 14:29:45 -0600

    added a custom action

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -7,6 +7,7 @@ from rest_framework import serializers
 from rest_framework import status
 from levelupapi.models import Event, Game, Gamer
 from django.contrib.auth.models import User
+from rest_framework.decorators import action
 
 
 class EventView(ViewSet):
@@ -29,14 +30,12 @@ class EventView(ViewSet):
 
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
-        
 
     def list(self, request):
         events = Event.objects.all()
         serializer = EventSerializer(
             events, context={'request': request}, many=True)
         return Response(serializer.data)
-
 
     def retrieve(self, request, pk=None):
         try:
@@ -51,7 +50,6 @@ class EventView(ViewSet):
         event = Event.objects.get(pk=pk)
         event.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
-
 
     def update(self, request, pk=None):
         event = Event.objects.get(pk=pk)
@@ -85,3 +83,41 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ('id', 'organizer', 'game', 'date', 'time', 'description')
+
+
+@action(methods=['post', 'delete'], detail=True)
+def signup(self, request, pk=None):
+    """Managing gamers signing up for events"""
+    # Django uses the `Authorization` header to determine
+    # which user is making the request to sign up
+    gamer = Gamer.objects.get(user=request.auth.user)
+
+    try:
+        # Handle the case if the client specifies a game
+        # that doesn't exist
+        event = Event.objects.get(pk=pk)
+    except Event.DoesNotExist:
+        return Response(
+            {'message': 'Event does not exist.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    # A gamer wants to sign up for an event
+    if request.method == "POST":
+        try:
+            # Using the attendees field on the event makes it simple to add a gamer to the event
+            # .add(gamer) will insert into the join table a new row the gamer_id and the event_id
+            event.attendees.add(gamer)
+            return Response({}, status=status.HTTP_201_CREATED)
+        except Exception as ex:
+            return Response({'message': ex.args[0]})
+
+    # User wants to leave a previously joined event
+    elif request.method == "DELETE":
+        try:
+            # The many to many relationship has a .remove method that removes the gamer from the attendees list
+            # The method deletes the row in the join table that has the gamer_id and event_id
+            event.attendees.remove(gamer)
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        except Exception as ex:
+            return Response({'message': ex.args[0]})
